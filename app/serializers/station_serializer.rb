e9f83f818050b19f56bab318064525409bf5d6cd
Mckomo <mckomo@gmail.com>
2015-12-18 00:35:57 +0100

    Removed measurements from the station object

--- app/serializers/station_serializer.rb
+++ app/serializers/station_serializer.rb
@@ -1,11 +1,7 @@
 class StationSerializer < ActiveModel::Serializer
 
-  attributes :id, :code, :name, :latitude, :longitude, :latest_measurements, :created_at, :updated_at
+  attributes :id, :code, :name, :latitude, :longitude, :created_at, :updated_at
 
-  def latest_measurements
-    object.measurements.latest.map { |m| MeasurementPreviewSerializer.new(m).attributes }
-  end
-
-  belongs_to :user,serializer: UserPreviewSerializer, key: 'creator'
+  belongs_to :user, serializer: UserPreviewSerializer, key: 'creator'
 
 end
