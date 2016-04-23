1afb5bed2c088fa93ef253aa6dc5b5d27ef1388c
Mckomo <mckomo@gmail.com>
2016-04-23 17:45:13 +0200

    Removed devise from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,9 @@
 class User < ActiveRecord::Base
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :confirmable, :validatable
-
   has_many :stations
   has_many :measurements
 
   validates :name, presence: true, length: { maximum: 64 }
   validates :email, presence: true
-
-  alias :active? :confirmed?
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe User, :type => :model do
 
   subject { create(:user) }
 
-  it 'is inactive by default' do
-    expect(subject.active?).to be(false)
-  end
-
 end
