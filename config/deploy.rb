f34ba0974232ab3b79a6f2aee32e437d659486d9
mckomo <mckomo@gmail.com>
2016-10-12 19:05:02 +0200

    Capistrano deploy config update

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :repo_url, 'git@github.com:mckomo/AirMonitor.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', '.rbenv-vars')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'newrelic.yml', '.rbenv-vars')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,6 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-server 'air.northeurope.cloudapp.azure.com', user: 'mckomo', roles: %w{app db web}
+server 'air-monitor.pl', user: 'mckomo', roles: %w{app db web}
 
 set :deploy_to, '/var/www/air-monitor-api'
